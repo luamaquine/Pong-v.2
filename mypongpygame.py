9da14eaa144b79d4dc209e76661b7fa9c847f445
lscantelbury <lhsda.snf20@uea.edu.br>
2021-05-21 22:21:21 -0400

    Adding Collision With Walls & Ball's Movement

--- mypongpygame.py
+++ mypongpygame.py
@@ -26,8 +26,8 @@ paddle_1_y = 560
 ball = pygame.image.load("assets/ball.png")
 ball_x = 640
 ball_y = 360
-ball_dx = 5
-ball_dy = 5
+ball_dx = 3
+ball_dy = 3
 
 # Variable for the following while loop
 keepplaying = True
@@ -57,17 +57,22 @@ while keepplaying:
     ball_y = ball_y + ball_dy
 
     # ball collision with upper wall
-    if ball_y > 590:
+    if ball_y <= 10:
         ball_dy *= -1
 
-    # ball collision with upper wall
-    if ball_y > 590:
-        ball_dy *= -1
+    # ball collision with right wall
+    if ball_x > 780:
+        ball_dx *= -1
     
-    # ball collision with upper wall
-    if ball_y > 590:
-        ball_dy *= -1
+    # ball collision with left wall
+    if ball_x <= 0:
+        ball_dx *= -1
 
+    # ball collision with the player 1 's paddle
+        if ball_y == 535:
+            if player_1_x < ball_y + 25:
+                if player_1_y + 150 > ball_y:
+                    ball_dy *= -1
 
     screen.blit(paddle_1, (325, paddle_1_y))
     screen.blit(ball, (ball_x, ball_y))
